e0d1b38ad636689f6d0a390b235beff2812960d8
FelipeBoos <felipeboos18@gmail.com>
2026-03-19 09:56:49 -0300

    feat(frontend/preco-page): display simulation result in modal

--- frontend/src/app/features/estrategias-preco/estrategias-preco/estrategias-preco.ts
+++ frontend/src/app/features/estrategias-preco/estrategias-preco/estrategias-preco.ts
@@ -4,10 +4,12 @@ import { Modal } from '../../../shared/components/modal/modal/modal';
 import { EstrategiaPrecoService, EstrategiaPrecoRequest, EstrategiaPrecoResponse } from '../services/estrategia-preco.service';
 import { ProdutoService, ProdutoResponse } from '../../produtos/services/produto.service';
 import { HttpErrorResponse } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
+import { ChangeDetectorRef } from '@angular/core';
 
 @Component({
   selector: 'app-estrategias-preco',
-  imports: [FormsModule, Modal],
+  imports: [FormsModule, Modal, CommonModule],
   templateUrl: './estrategias-preco.html',
   styleUrl: './estrategias-preco.css',
 })
@@ -17,11 +19,16 @@ export class EstrategiasPreco implements OnInit {
   percentualImposto: number | null = null;
 
   exibirFormulario = false;
+  exibirResultadoSimulacao = false;
   estrategiasPreco = signal<EstrategiaPrecoResponse[]>([]);
+
+  resultadoSimulacao: EstrategiaPrecoResponse | null = null;
 
   produtos = signal<ProdutoResponse[]>([]);
 
-  constructor(private estrategiaPrecoService: EstrategiaPrecoService, private produtoService: ProdutoService) {}
+  constructor(private estrategiaPrecoService: EstrategiaPrecoService, private produtoService: ProdutoService,
+    private cdr: ChangeDetectorRef
+  ) {}
 
   ngOnInit(): void {
     this.carregarProdutos();
@@ -39,24 +46,15 @@ export class EstrategiasPreco implements OnInit {
     });
   }
 
-  botaoSimular(): void {
-    this.abrirSimulacao();
-  }
-
   botaoFiltrar(): void {
     alert('Botão filtrar: ainda não implementado');
   }
 
-  abrirSimulacao(): void {
-    this.exibirFormulario = true;
-  }
-
-  fecharSimulacao(): void {
-    this.exibirFormulario = false;
-  }
-
   resetarFormulario(): void {
-
+    this.produtoId = null;
+    this.margemLucro = null;
+    this.percentualImposto = null;
+    this.resultadoSimulacao = null;
   }
 
   carregarProdutos(): void {
@@ -72,6 +70,7 @@ export class EstrategiasPreco implements OnInit {
   }
 
   simularEstrategiaDePreco() {
+
     if (
       this.produtoId === null ||
       this.margemLucro === null ||
@@ -89,12 +88,25 @@ export class EstrategiasPreco implements OnInit {
 
     this.estrategiaPrecoService.simularEstrategiaPreco(estrategiaPreco).subscribe({
       next: (resposta) => {
+        this.resultadoSimulacao = { ...resposta };
+        this.exibirResultadoSimulacao = true;
+        this.cdr.detectChanges();
         console.log('Resultado da simulação: ', resposta);
       },
       error: (erro) => {
         console.error('Erro na simulação: ', erro);
       }
     });
+  }
+
+  botaoSimular() {
+    this.exibirFormulario = true;
+  }
+
+  fecharSimulacao() {
+    this.exibirFormulario = false;
+    this.exibirResultadoSimulacao = false;
+    this.resetarFormulario();
   }
 
   excluirEstrategiaPreco(id: number): void {
